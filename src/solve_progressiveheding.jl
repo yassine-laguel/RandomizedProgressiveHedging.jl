20a91231e9da64056ad407e7a3967129da4eecd9
Gilles <gilles.bareilles@gmail.com>
2019-06-20 10:46:14 +0200

    improve ph solve

--- src/solve_progressiveheding.jl
+++ src/solve_progressiveheding.jl
@@ -19,24 +19,40 @@ end
 
 
 """
-    solve_progressivehedging(pb::Problem)
+    x = solve_progressivehedging(pb::Problem)
 
-Run the classical Progressive Hedging scheme on problem `pb`.
+Run the classical Progressive Hedging scheme on problem `pb`. 
+
+Stopping criterion is based on primal dual residual, maximum iterations or time can also be set.
+
+Return a feasible point `x`
+
+## Keyword arguments:
+- `ϵ_primal`: Tolerance on primal residual.
+- `ϵ_dual`: Tolerance on dual residual.
+- `μ`: Regularization parameter.
+- `tlim`: Limit time spent in computations.
+- `maxiter`: Maximum iterations.
+- `printlev`: if 0, mutes output.
+- `print_step`: number of iterations skipped between each print and logging.
+- `hist`: if not nothing, will record:
+    + `:functionalvalue`: array of functional value indexed by iteration,
+    + `:time`: array of time at the end of each iteration, indexed by iteration,
+    + `:dist_opt`: if dict has entry `:approxsol`, array of distance between iterate and `hist[:approxsol]`, indexed by iteration.
+    + `:logstep`: number of iteration between each log.
 """
-function solve_progressivehedging(pb::Problem; kwargs...)
-    println("--------------------------------------------------------")
-    println("--- Progressive Hedging - sequential")
-    println("--------------------------------------------------------")
+function solve_progressivehedging(pb::Problem; ϵ_primal = 1e-3,
+                                               ϵ_dual = 1e-3,
+                                               μ = 3,
+                                               tlim = 3600,
+                                               maxiter = 1e3,
+                                               printlev = 1,
+                                               printstep = 1,
+                                               hist::Union{OrderedDict{Symbol, Any}, Nothing}=nothing)
+    printlev>0 && println("--------------------------------------------------------")
+    printlev>0 && println("--- Progressive Hedging")
+    printlev>0 && println("--------------------------------------------------------")
     
-    # parameters
-    μ = 3
-    params = Dict(
-        :max_iter => 100,
-        :print_step => 10,
-        :ϵ_prim => 1e-3,
-        :ϵ_dual => 1e-3,
-    )
-
     # Variables
     nstages = pb.nstages
     nscenarios = pb.nscenarios
@@ -46,14 +62,16 @@ function solve_progressivehedging(pb::Problem; kwargs...)
     x = zeros(Float64, nscenarios, n)
     u = zeros(Float64, nscenarios, n)
     primres = dualres = Inf
-    
-    # Initialization
-    # y = subproblems per scenario
-    # nonanticipatory_projection!(x, pb, y)
+
+    tinit = time()
+    !isnothing(hist) && (hist[:functionalvalue] = Float64[])
+    !isnothing(hist) && (hist[:time] = Float64[])
+    !isnothing(hist) && haskey(hist, :approxsol) && (hist[:dist_opt] = Float64[])
+    !isnothing(hist) && (hist[:logstep] = printstep)
 
     it = 0
-    @printf " it   primal res        dual res            dot(x,u)   objective\n"
-    while !(primres < params[:ϵ_prim] && dualres < params[:ϵ_dual]) && it < params[:max_iter]
+    printlev>0 && @printf " it   primal res        dual res            dot(x,u)   objective\n"
+    while !(primres < ϵ_primal && dualres < ϵ_dual) && it < maxiter && time()-tinit < tlim
         u_old = copy(u)
 
         # Subproblem solves
@@ -71,11 +89,15 @@ function solve_progressivehedging(pb::Problem; kwargs...)
         # invariants, indicators
         primres = norm(pb, x-y)
         dualres = (1/μ) * norm(pb, u - u_old)
-        if mod(it, params[:print_step]) == 0
+        if mod(it, printstep) == 0
             objval = objective_value(pb, x)
             dot_xu = dot(pb, x, u)
-        
-            @printf "%3i   %.10e  %.10e   % .3e % .16e\n" it primres dualres dot_xu objval
+
+            printlev>0 && @printf "%3i   %.10e  %.10e   % .3e % .16e\n" it primres dualres dot_xu objval
+
+            !isnothing(hist) && push!(hist[:functionalvalue], objval)
+            !isnothing(hist) && push!(hist[:time], time() - tinit)
+            !isnothing(hist) && haskey(hist, :approxsol) && push!(hist[:dist_opt], norm(hist[:approxsol] - x))
         end
 
         it += 1
@@ -85,7 +107,8 @@ function solve_progressivehedging(pb::Problem; kwargs...)
     objval = objective_value(pb, x)
     dot_xu = dot(pb, x, u)
 
-    @printf "%3i   %.10e  %.10e   % .3e % .16e\n" it primres dualres dot_xu objval
+    printlev>0 && @printf "%3i   %.10e  %.10e   % .3e % .16e\n" it primres dualres dot_xu objval
+    printlev>0 && println("Computation time: ", time() - tinit)
 
     return x
 end
