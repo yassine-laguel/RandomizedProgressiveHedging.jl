d0b2c6afe8fe5b29bdd992eff33ad82dd9e2cd09
Gilles <gilles.bareilles@gmail.com>
2019-06-25 14:54:18 +0200

    further subpbproj optimization

--- src/projections.jl
+++ src/projections.jl
@@ -2,6 +2,7 @@
     nonanticipatory_projection!(x::Matrix{Float64}, pb::Problem, y::Matrix{Float64})
 
 Store in `x` the projection of `y` on the non-anticipatory subspace associated to problem `pb`.
+Note: this function has been fairly optimized. Apply changes with caution.
 """
 function nonanticipatory_projection!(x::Matrix{Float64}, pb::Problem, y::Matrix{Float64})
     @assert size(x) == size(y) "nonanticipatory_projection!(): input and output arrays should have same size"
@@ -48,13 +49,13 @@ Note: this function has been fairly optimized. Apply changes with caution.
 """
 function get_averagedtraj!(averaged_traj::AbstractVector, pb::Problem, z::Matrix, id_scen::ScenarioId)
     nstages = pb.nstages
-    n = sum(length.(pb.stage_to_dim))
-    
-    averaged_traj .= 0
-    
+    n = 0
+    for stagedims in pb.stage_to_dim n+= length(stagedims) end
+
+
     scentree = pb.scenariotree
     stage = 1
-    id_curnode = scentree.idrootnode
+    id_curnode = RPH.STreeNodeId(scentree.idrootnode)
 
     while stage <= scentree.depth
         ## Get scenarios, dimension for current stage
@@ -62,23 +63,30 @@ function get_averagedtraj!(averaged_traj::AbstractVector, pb::Problem, z::Matrix
         stage_dims = pb.stage_to_dim[stage]
 
         ## update x section with average
-        sum_probas = sum(pb.probas[i] for i in scen_set)
+        sum_probas = 0.0
         for i in scen_set
-            for stage_dim in stage_dims
-                @inbounds averaged_traj[stage_dim] += pb.probas[i] * z[i, stage_dim] / sum_probas
+            @inbounds sum_probas += pb.probas[i]
+        end
+
+        for stage_dim in stage_dims
+            val = 0.0
+            for i in scen_set
+                @inbounds val += pb.probas[i] * z[i, stage_dim]
             end
+            @inbounds averaged_traj[stage_dim] = val / sum_probas
         end
 
         ## find node of following stage
         stage += 1
-        id_nextnode = nothing
+        id_nextnode::RPH.STreeNodeId, hasfound_nextnode = 0, false
         for id_child in scentree.vecnodes[id_curnode].childs
             if id_scen in scentree.vecnodes[id_child].scenarioset
                 id_nextnode = id_child
+                hasfound_nextnode = true
                 break
             end
         end
-        isnothing(id_nextnode) && stage < scentree.depth && @error "Tree dive, node of depth $stage not found."
+        !hasfound_nextnode && stage <= scentree.depth && begin @error "Tree dive, node of depth $stage not found."; return end
         id_curnode = id_nextnode
     end
     return
