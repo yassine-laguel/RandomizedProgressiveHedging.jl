ab9d9917f19272e45a8078448172068da41bfc30
Gilles <gilles.bareilles@gmail.com>
2019-06-25 14:58:49 +0200

    CI fix ?

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,10 @@
 using Test
-using RPH
 
+using Distributed
+addprocs(1)
+
+@everywhere push!(LOAD_PATH, pwd())
+@everywhere println(pwd())
+@everywhere using RPH, JuMP
 
 include("test_simpleex.jl")
